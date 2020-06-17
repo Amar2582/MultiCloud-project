48a70a8512f9b04539d1d4716ad0b686b3f537a1
Amar2582 <64667387+Amar2582@users.noreply.github.com>
2020-06-18 03:06:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-Welcome to Amardeep Kumar's page
-PUBG partner
+Welcome to Amardeep Kumar's page.\n SQUAD
 <pre>
 <html>
 <body>
 <?php
-print "ifconfig";
+print "https://amar2582.s3.ap-south-1.amazonaws.com/Squd.jpg";
 ?>
 </body>
 </html>
